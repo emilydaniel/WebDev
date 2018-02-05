ed666042c98d7da7d3b7c0cbd6b939bef971b1e8
Emily D <emdaniel@ccs.neu.edu>
2018-02-05 02:50:42 -0500

    Finished calculator

--- calc/calc/lib/calc.ex
+++ calc/calc/lib/calc.ex
@@ -1,10 +1,124 @@
 defmodule Calc do
     def main() do
-        IO.puts "Enter expression to calculate: "    
-        IO.gets()
+        case IO.gets("Enter expression to calculate \n") do
+            {:error, reason} ->
+                IO.puts "Error: #{reason}"
+        
+            str ->
+                str
+                    |> eval()
+                    |> IO.puts()
+                main()
+        end
     end
 
+    #String -> Number
     def eval(str) do
+        try do
+        str
+            |> String.trim()
+            |> separate_numbers()
+            |> String.split()
+            |> eval_list()
+            catch
+                x -> "Error: #{x}"
+            end
+    end
+   
+    #List -> Number 
+    defp eval_list(li) do
+        cond do
+            Enum.find(li, fn(x) -> x == "(" end) ->
+                parenIndexL = Enum.find_index(li, fn(x) -> x == "(" end)
+                    parenIndexR = match_paren(parenIndexL, li)
+                    leftlist = Enum.slice(li, 0..(parenIndexL - 1))
+                    middleVal = eval_list(Enum.slice(li, (parenIndexL + 1)..(parenIndexR - 1)))
+                    middlelist = List.insert_at([], 0, Integer.to_string(middleVal))
+                    rightlist = Enum.slice(li, (parenIndexR + 1)..(Enum.count(li) - 1))
+                    leftlist
+                        |> Enum.concat(middlelist)
+                        |> Enum.concat(rightlist)
+                        |> eval_list()
 
+            Enum.find(li, fn(x) -> x == "+" end) ->
+                addIndex = Enum.find_index(li, fn(x) -> x == "+" end)
+                leftlist = li
+                            |> Enum.slice(0..addIndex - 1)
+                            |> eval_list
+                rightlist = li
+                            |> Enum.slice((addIndex + 1)..(Enum.count(li) - 1))
+                            |> eval_list
+                leftlist + rightlist
+
+            Enum.find(li, fn(x) -> x == "-" end) ->
+                subIndex = Enum.find_index(li, fn(x) -> x == "-" end)
+                leftlist = li
+                            |> Enum.slice(0..subIndex - 1)
+                            |> eval_list
+                rightlist = li
+                            |> Enum.slice((subIndex + 1)..(Enum.count(li) - 1))
+                            |> eval_list
+                leftlist - rightlist
+
+            Enum.find(li, fn(x) -> x == "*" end) ->
+                multIndex = Enum.find_index(li, fn(x) -> x == "*" end)
+                leftlist = li
+                            |> Enum.slice(0..multIndex - 1)
+                            |> eval_list
+                rightlist = li
+                            |> Enum.slice((multIndex + 1)..(Enum.count(li) - 1))
+                            |> eval_list
+                leftlist * rightlist
+
+            Enum.find(li, fn(x) -> x == "/" end) ->
+                divIndex = Enum.find_index(li, fn(x) -> x == "/" end)
+                leftlist = li
+                            |> Enum.slice(0..divIndex - 1)
+                            |> eval_list
+                rightlist = li
+                            |> Enum.slice((divIndex + 1)..(Enum.count(li) - 1))
+                            |> eval_list
+                leftlist / rightlist
+
+            true ->
+                try do
+                li
+                    |> List.first()
+                    |> String.to_integer()
+                catch
+                    x -> "Error #{x}"
+                end
+        end
+    end
+
+    #Number List -> Number
+    defp match_paren(parenIndex, li) do
+        firstClose = Enum.find_index(li, fn(x) -> x == ")" end)
+        lastClose = Enum.count(li) - Enum.find_index(Enum.reverse(li), fn(x) -> x == ")" end)
+        cond do
+            firstClose == lastClose -> 
+                lastClose
+            firstClose < parenIndex -> 
+                error = "Error: No matching opening paren for closing paren" 
+                throw(error)
+                IO.puts(error)
+            firstClose > parenIndex ->
+                firstClose
+            true ->
+                error = "Error: No matching closing paren for opening paren"
+                throw(error)
+                IO.puts(error)
+        end
+    end
+
+    #String -> String
+    defp separate_numbers(str) do
+        str
+            |> String.replace("(", " ( ")
+            |> String.replace(")", " ) ")
+            |> String.replace("+", " + ")
+            |> String.replace("-", " - ")
+            |> String.replace("*", " * ")
+            |> String.replace("/", " / ")
     end
 end
--- calc/calc/test/calc_test.exs
+++ calc/calc/test/calc_test.exs
@@ -2,7 +2,97 @@ defmodule CalcTest do
   use ExUnit.Case
   doctest Calc
 
-  test "greets the world" do
-    assert Calc.hello() == :world
-  end
+    #simple parsing
+    test "simple addition" do
+        assert Calc.eval("2 + 3") == 5
+    end
+
+    test "simple multiplication" do
+        assert Calc.eval("3 * 4") == 12
+    end
+
+    test "simple division" do
+        assert Calc.eval("12 / 3") == 4
+    end
+
+    test "simple subtraction" do
+        assert Calc.eval("4 - 2") == 2
+    end
+
+    #no space parsing
+    test "no space addition" do
+        assert Calc.eval("3+2") == 5
+    end
+
+    test "no space multiplication" do
+        assert Calc.eval("4*1") == 4
+    end
+
+    test "no space division" do
+        assert Calc.eval("8/2") == 4
+    end
+
+    test "no space subtraction" do
+        assert Calc.eval("6-7") == -1
+    end
+
+    #order of operations
+    test "simple order of ops" do
+        assert Calc.eval("3 * 2 + 3") == 9
+    end
+
+    test "simple order of ops 2" do
+        assert Calc.eval("4 - 2 / 2") == 3
+    end
+
+    test "simple order of ops 3" do
+        assert Calc.eval("3 + 4 / 2") == 5
+    end
+
+    test "med order of ops" do
+        assert Calc.eval("3 * 2 - 6 + 2") == 2
+    end
+    
+    test "med order of ops 2" do
+        assert Calc.eval("3 / 3 + 5 * 2") == 11
+    end
+
+    test "hard order of ops" do
+        assert Calc.eval("3 / 4 * 2 - 2") == -0.5
+    end
+
+    test "hard order of ops 2" do
+        assert Calc.eval("3 - 2 + 2 * 2 + 4") == 9
+    end
+
+    test "hard order of ops 3" do
+        assert Calc.eval("5 * 3 / 2 + 4 * 2 * 3 - 5") == 26.5
+    end
+    
+    # simple parens
+    test "simple parens" do
+        assert Calc.eval("3 + (4 - 2)") == 5
+    end
+
+    test "simple parens 2" do
+        assert Calc.eval("(2 * 2) - 2") == 2
+    end
+
+    #multiple parens
+    test "multiple parens" do
+        assert Calc.eval("(2 - 5) + 4 * (3 - 2)") == 1
+    end
+
+    test "multiple parens 2" do
+        assert Calc.eval("3 * (2 + 1) - 5 + (4 / 2) - 2") == 4
+    end
+
+    #nested parens
+    test "nested parens" do
+        assert Calc.eval("3 * (3 * (2 - 3))") == -9
+    end
+
+    test "nested parens 2" do
+        assert Calc.eval("(4 - 2 + (3 - 4 * 3) + 6) * 8") == -8
+    end
 end
